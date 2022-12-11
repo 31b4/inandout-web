9b7845738835580acd5b66473f72f8f139161eaf
bence <75566095+31b4@users.noreply.github.com>
2022-12-11 22:18:10 +0100

    Add files via upload

---
+++ tradingview.js
@@ -0,0 +1,28 @@
+new TradingView.widget(
+  {
+  "container_id": "technical-analysis-chart-demo",
+  "width": "100%",
+  "height": "700px",
+  "autosize": false,
+  "symbol": "EURJPY",
+  "interval": "4H",
+  "timezone": "exchange",
+  "theme": "dark",
+  "style": "1",
+  "toolbar_bg": "#f1f3f6",
+  "withdateranges": false,
+  "hide_side_toolbar": false,
+  "allow_symbol_change": true,
+  "save_image": false,
+  "studies": [
+    
+    
+    "MASimple@tv-basicstudies"
+  ],
+  "show_popup_button": true,
+  "popup_width": "1000",
+  "popup_height": "650",
+  "locale": "en"
+}
+  );
+
